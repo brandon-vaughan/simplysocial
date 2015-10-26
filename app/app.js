22c3b4156f4402794891646bc7e6cd14fe4aec65
Brandon Vaughan <brandon@nvite.com>
2015-10-26 07:52:37 -0400

    removing unused dep

--- app/app.js
+++ app/app.js
@@ -7,11 +7,9 @@
 import angular from 'angular';
 import ngSanitize from 'angular-sanitize';
 import ngRoute from 'angular-route';
-import ngRedux from 'ng-redux';
 
 export default angular
   .module('app', [
     ngSanitize,
-    ngRoute,
-    ngRedux,
+    ngRoute
   ]).name;
